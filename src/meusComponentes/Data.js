1424ce43c3d585be785dfc5839cce79aaa272dbd
AriCampos18 <arianelimacampos16@gmail.com>
2024-08-23 09:25:26 -0300

    Fatorando componente data

--- src/meusComponentes/Data.js
+++ src/meusComponentes/Data.js
@@ -7,15 +7,33 @@ export default class Data extends Component{
         this.props = props;
         this.dataAtual = new Date().toLocaleString();
     }
+    
+    pegaDataDe(timeZone){
+        const dataAtual = new Date();
+        let timeZoneFromDB = parseInt(timeZone); 
+        let diferencaTempo = timeZoneFromDB * 60 + dataAtual.getTimezoneOffset();
+        let milisecundos = parseInt(dataAtual.getTime() + diferencaTempo * 60 * 1000)
+        console.log(milisecundos)
+        return Date(milisecundos);
+    }
 
     //fase de montagem
-    componentDidMount(){ 
+    componentDidMount(){
         console.log("O componente foi montado.");
-        
+        //Não é permitido atualizar o estado do componente de forma direta
+        //this.state =  ...
+        this.setState({
+           dataAtual: new Date().toLocaleString() 
+        });
     }
 
     componentDidUpdate(){
-        console.log("O componente foi atualizado.");
+        console.log("O componente foi atualizado");
+        setTimeout(()=>{
+        this.setState({
+                dataAtual: this.pegaDataDe(this.props.timeZone).toLocaleString() 
+        });
+        },1000);
     }
 
     //sobrescrita de metodo
